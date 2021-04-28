82bdf956099ba9e1319d3cc9e6bcc050fd800757
Emon-Alam <66877658+Emon-Alam@users.noreply.github.com>
2021-04-28 13:47:55 +0600

    diary

--- Front End/src/components/Navbar.js
+++ Front End/src/components/Navbar.js
@@ -4,10 +4,10 @@ const Navbar = ()=>{
     return (
         <div>
             <h1>Digital Diary</h1>
-            <Link to='/'>Home</Link> |  
-            <Link to='/add'>Create event</Link> | 
-            <Link to='/userlist'>User list</Link>
+            <Link to='/'>Home</Link> |
+            <Link to='/add'>Create event</Link> |
+            <Link to='/userlist'>User Event list</Link>
         </div>
-    );  
+    );
 }
 export default Navbar;
